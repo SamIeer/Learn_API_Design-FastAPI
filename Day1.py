f8c7211913d17f411146c6a3b77d8b5a7fa79dcd
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-30 22:36:21 +0530

    Getting started

--- Day1.py
+++ Day1.py
@@ -1,2 +1,19 @@
-# Learning about whats API is and how its woerk
-# Day 1 
+# Learning about whats API is and how its work
+'''
+An API, which stands for application programming interface, is a set of protocols that enable different
+software components to cummunicate and transfer data.
+Developers use APIs to bridge the gaps between samall, discrete chunks of code 
+to create applications that are poweful, resilient, secure, abd able to meet uset needs.
+HISTORY OF APIs
+Commercial APIs 
+Social media APIs
+Cloud APIs
+APIs for mobile apps
+APIs for connected devices
+
+HOW DO APUIs WORK : APIs work by sharing data between applications, systems ,and devices. This happens through request and response cycle.
+The request is sent to the API, which retrieves the data and returns it to the user
+
+1. API client :
+
+'''
