e156d41bc793cff51b1f87884205f610e5d78ed9
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-31 18:07:29 +0530

    architectural styles

--- Day2.py
+++ Day2.py
@@ -1,2 +1,24 @@
-# Learning about different types of API
-# Day 2 comming here 
+# API Architectural styles 
+"""
+1. REST : most populat api architecture for transferring data over the internet. 
+In a restful context, resources are accessible via endpoints, and operations are performed on those resources
+with standard  HTTP methods such as GET, POST, PUT and DELETE
+
+2. Soap : stands for simple object access protocol, uses XML to transfer highly structured messages between a client and server.
+
+3. GraphQL : Open source query language that enables clients to interact with a single API endpoint to retrieve the excat data they need,
+without chaining multiple requests together
+
+4. webhooks : webhooks are used to implement event-driven architectures, in ehich requests are automatically sent in response to event-based tiggers.
+
+5. gRPC : RPC stands for remote procedure call, and grpc apis were originated by google
+
+What are some common API use cases?
+- Integrating with inernal and external systems
+- Adding to enhancing functionality
+- Connecting IoT devices
+- Creating more scalable systems
+- Reducing costs
+- Improving organizational security and governace
+
+"""
