670e2a19b740e1e81b0eea634b81505f19162045
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-15 13:38:03 +0530

    Endpoint for creating todo

--- Database_connection/main.py
+++ Database_connection/main.py
@@ -65,3 +65,20 @@ def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+# Creating a New To-Do
+from pydantic import BaseModel, Field
+
+# Request model for creating a new todo 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, it=6)
+    complete: bool
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
