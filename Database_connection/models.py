d39ae3fba2fb74f53811144bd6a547966b84d443
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-16 22:27:45 +0530

    auth schema

--- Database_connection/models.py
+++ Database_connection/models.py
@@ -1,6 +1,18 @@
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, Boolean
+from sqlalchemy import String, Integer, Boolean, Column, ForeignKey
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String) # store roles like "admin" or "user"
 
 class Todos(Base):
     __tablename__ = 'todos' # Name of the table in the database
