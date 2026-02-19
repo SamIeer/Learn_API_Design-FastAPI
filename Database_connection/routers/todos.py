70e2e2dda18ebc3fdd3374df442836bc013a213b
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-19 22:16:12 +0530

    adding user authenticity

--- Database_connection/routers/todos.py
+++ Database_connection/routers/todos.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from models import Todos
 from database import sessionLocal
+from .auth import get_current_user, authenticate_user
 
 router = APIRouter()
 
@@ -39,7 +40,9 @@ def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 # POST: create new todo
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo(db: db_dependency, todo_request: TodoRequest):
+def create_todo(user: authenticate_user, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model)
     db.commit()
