7d89d7477356ce4e949b25bf37bdd3f03f30862d
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-22 19:30:49 +0530

    order rule

--- Database_connection/Dependencies.py
+++ Database_connection/Dependencies.py
@@ -58,3 +58,55 @@ db_dependency = Annotated[Session, Depends(get_db)]
 #  Now use it:
 async def endpoint(db: db_dependency):
     pass
+
+'''
+How FastAPI processes parameters
+'''
+# PAth Parameters
+@app.get("/uisers/{user_id}")
+async def get_user(user_id: int): # <- Path parameter
+    pass
+
+# Dependencies
+@app.get("/users/")
+async def get_users(db: Session = Depends(get_db)): # <- Dependency
+    pass
+
+# Query parameter
+@app.get("/users/")
+async def  get_users(skip: int = 0, limit: int = 10 ): # <- Query parameter
+    pass
+
+# Request body
+@app.get("/users/")
+async def create_user(user: Usrcreate): # <- Request body
+    pass
+
+"""
+Order Rule : Why it matters
+"""
+# Wrong order 
+@app.get("/callback")
+async def auth_google(code: str = None, db: Session = Depends(get_db)):
+    pass
+
+# Correct order
+@app.get("/callback")
+async def auth_google(db: Session = Depends(get_db), code: str = None):
+    pass
+
+'''
+why?
+python requires non-default parameters to come before default parameters
+'''
+#py error
+def func(a=1,b): # can't put a non-default 'b' after default 'a'
+    pass
+def func(b, a=1): # non-default first, then defaults
+    pass
+
+'''
+Even though db: Session = Depends(get_db) looks like it has a default,
+FastAPI treats dependencies as non-default parameters internally. 
+So they must come before optional query parameters.
+'''
