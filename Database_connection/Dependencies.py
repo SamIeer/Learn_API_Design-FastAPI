36f9c0387ff47c46a8cbcac3f0e852153df4f4ff
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-22 17:50:32 +0530

    With dependencies

---
+++ Database_connection/Dependencies.py
@@ -0,0 +1,28 @@
+# FastAPI Dependencies: A complete Guide 
+# what are Dependencies
+'''
+Avoid code repetition
+Share logic across multiple endpoints 
+manage resources (like database connections)
+Handle authentication and authorization
+Validate requests
+Think of dependencies as "helpers" that FastAPI automatically calls before running your endpoint.
+'''
+# With Dependencies 
+# Define dependency once 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#use it everywhere
+@app.get("/users/")
+async def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
+@app.get("/posts/")
+async def get_posts(db: Session = Depends(get_db)):
+    return db.query(Post).all
+
