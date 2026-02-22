9afc4ee91256faaa8c343e083a3c6349d44b80ad
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-22 19:04:50 +0530

    using alias

--- Database_connection/Dependencies.py
+++ Database_connection/Dependencies.py
@@ -34,3 +34,27 @@ from sqlalchemy.orm import Session
 @app.get("/users/")
 async def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+## -> 2 Authentication
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # Verify token and return user
+    user = verify_token(token)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user
+
+@app.get("/profile/")
+async def get_profile(currencent_user: User = Depends(get_current_user)):
+    return {"username": currencent_user.username}
+
+#->3 Type Alias (Cleaner code)
+# instead of writing this wverywhere:
+async def endpoint(db: Session = Depends(get_db)):
+    pass
+
+# Create a type alias:
+db_dependency = Annotated[Session, Depends(get_db)]
+
+#  Now use it:
+async def endpoint(db: db_dependency):
+    pass
