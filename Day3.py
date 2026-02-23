5843a6db68dd0ae416c54e602d586dd22746ac1f
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-02-23 21:17:45 +0530

    Types of testing

--- Day3.py
+++ Day3.py
@@ -1,2 +1,9 @@
-# Getting to know fastAPI and some endpoints
-# Day 3
+#Testing Overview
+'''
+What is Testing 
+it is a way for us to make sure our application working as intended
+part of software development lifecycle that aims to identify:
+-> Bugs -> Error -> Deffects
+Meets user requirements and specifications
+Ensuring software is high quality, reliabe, secure and user friendly
+'''
